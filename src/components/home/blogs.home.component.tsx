b6a7d1030c3886bd37ac36b2fd375052359d0e36
awaisali <awais.ali@tintash.com>
2022-08-30 16:14:31 +0500

    feat: blogs-content

---
+++ src/components/home/blogs.home.component.tsx
@@ -0,0 +1,49 @@
+import Contents from "./data.json";
+const BlogsContent = () => {
+  return (
+    <div className="home-contents h-full w-5/6 m-auto flex flex-col laptop:ml-20 laptop:w-4/5">
+      <div className="latest flex flex-col items-center tablet:justify-start tablet:items-start">
+        <hr className="bg-primary mt-10 h-1 w-5"></hr>
+        <h1 className="font-light text-xl leading-6 text-darkgrey font-lexend">
+          Latest
+        </h1>
+      </div>
+      <div className="blogs flex flex-col">
+        {Contents.map((data) => {
+          return (
+            <div key={data.id} className="blog my-5">
+              <h1 className="date hidden font-semibold text-2xl leading-7 not-italic font-lexend tablet:block">
+                {data.date}
+              </h1>
+              <h1 className="title mt-3 font-medium font-serif text-2xl leading-8 not-italic text-primary tablet:text-4xl">
+                {data.title}
+              </h1>
+              <br />
+              <span className="content font-normal text-base leading-5 not-italic font-lexend tablet:text-xl tablet:leading-6">
+                {data.content.length > 250
+                  ? data.content.slice(0, 250)
+                  : data.content}{" "}
+                {data.content.length > 250 && (
+                  <a href="">
+                    <span className="read-more font-light text-xl leading-6 not-italic font-lexend text-primary">
+                      ...read more
+                    </span>
+                  </a>
+                )}
+              </span>
+              <div className="userName mt-5 flex justify-between items-center">
+                <h1 className="date font-semibold text-base leading-5 not-italic font-lexend tablet:hidden">
+                  {data.date}
+                </h1>
+                <h1 className="text-[#A5A5A5] font-lexend font-light text-base leading-5">
+                  {data.username}
+                </h1>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+export default BlogsContent;
--- src/components/home/home.component.tsx
+++ src/components/home/home.component.tsx
@@ -1,3 +1,4 @@
+import BlogsContent from "./blogs.home.component";
 import NavBar from "./navbar.home.component";
 const Home = () => {
   return (
@@ -5,6 +6,9 @@ const Home = () => {
       <div className="home-page h-full flex flex-col-reverse laptop:flex-row">
         <div className="nav-bar laptop:w-1/12 laptop:h-full">
           <NavBar />
+        </div>
+        <div className="home-content overflow-y-auto laptop:w-11/12">
+          <BlogsContent />
         </div>
       </div>
     </>
